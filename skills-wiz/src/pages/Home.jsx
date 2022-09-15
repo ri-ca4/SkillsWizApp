44269d39521f5fc0abb41e8d90e38d5c085353cc
ri-ca4 <riley.de12@gmail.com>
2022-09-15 10:40:14 -0400

    add popup component

--- skills-wiz/src/pages/Home.jsx
+++ skills-wiz/src/pages/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PopUp from "../components/PopUp";
 
 const Home = ()=>{
+    const [timedPopup, setTimedPopup] = useState(false);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setTimedPopup(true);
+        }, 3000);
+    }, []);
+
     return(
         <div>
             <p>home page</p>
+            <button><Link to='/main_app'>Start</Link></button>
+            <PopUp trigger={timedPopup} setTrigger={setTimedPopup}>
+                <h3>Pop-up</h3>
+            </PopUp>
         </div>
     )
 }
