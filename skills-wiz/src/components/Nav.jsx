d9387754d0ad8ff81ca902a5ebb6eed48b88572d
ri-ca4 <riley.de12@gmail.com>
2022-09-15 20:49:52 -0400

    imporve nav style and functionality

--- skills-wiz/src/components/Nav.jsx
+++ skills-wiz/src/components/Nav.jsx
@@ -3,21 +3,26 @@ import { Link } from "react-router-dom";
 
 
 const Links = (props)=>{
-    const {navVisibility} = props
+    const {navVisibility, setNavVisibility} = props
+
+    const handleClick = ()=>{
+        setNavVisibility(v => !v);
+    }
+
     return(
         ( navVisibility &&
         <nav>
             <ul>
-                <li className="nav-item">
+                <li className="nav-item" onClick={handleClick}>
                     <Link to='/'>Home</Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" onClick={handleClick}>
                     <Link to='/about'>About</Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" onClick={handleClick}>
                     <Link to='/skills'>Skills</Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item" onClick={handleClick}>
                     <Link to='/resources'>Resources</Link>
                 </li>
             </ul>
@@ -43,7 +48,7 @@ const Nav = ()=>{
                 <span></span>
                 <span></span>
             </button>
-            <Links navVisibility={navVisibility}/>
+            <Links navVisibility={navVisibility} setNavVisibility={setNavVisibility}/>
         </div>
     )
 }
