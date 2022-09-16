6832ece3e40e0481afd3a44258c81d9609508731
ri-ca4 <riley.de12@gmail.com>
2022-09-16 12:44:16 -0400

    improve results display

--- skills-wiz/src/components/ResultsFooter.jsx
+++ skills-wiz/src/components/ResultsFooter.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import './css/results.css'
+
+const Buttons = ()=>{
+  return(
+  <div className='buttons'>
+    <button className='home'><Link to='/'>Home</Link></button>
+    <button className='skills'><Link to='/skills'>See Skills</Link></button>
+  </div>
+  )
+}
 
 const ResultsFooter = () => {
   return (
-    <div className='mindfulness'>
-    <h1>Remember</h1>
-    <h2>Core Mindfulness</h2>
-    <h3>Use these skills with all other skills</h3>
-    <h3>Practice all the time</h3>
-    <h3>Take hold of your mind!</h3>
-    <ul>
-        <li>States of mind: Emotion mind, Reasonable mind, Wise mind</li>
-        <li>"What" to do while mindful: Observe, Describe, Participate</li>
-        <li>"How" to do it: Non-judgementally, One-mindfully, Effectively</li>
-    </ul>
-    <button><Link to='/'>Home</Link></button>
-    <button><Link to='/skills'>See Skills</Link></button>
-  </div>
+    <div>
+      <div className='mindfulness'>
+      <h1>Remember</h1>
+      <h2>Core Mindfulness</h2>
+      <h3>Use these skills with all other skills</h3>
+      <h3>Practice all the time</h3>
+      <h3>Take hold of your mind!</h3>
+      <ul>
+          <li>States of mind: Emotion mind, Reasonable mind, Wise mind</li>
+          <li>"What" to do while mindful: Observe, Describe, Participate</li>
+          <li>"How" to do it: Non-judgementally, One-mindfully, Effectively</li>
+      </ul>
+      </div>
+      <Buttons />
+    </div>
   )
 }
 
