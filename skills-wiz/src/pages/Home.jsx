5d22989b0d09aceac482fbc65bd59af59c3f38f0
ri-ca4 <riley.de12@gmail.com>
2022-09-16 17:24:01 -0400

    changes to style

--- skills-wiz/src/pages/Home.jsx
+++ skills-wiz/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import PopUp from "../components/PopUp";
 import useSessionStorage from "../hooks/useSessionStorage";
+import "../App.css"
 
 const Home = ()=>{
     const [timedPopup, setTimedPopup] = useState(false);
@@ -18,9 +19,9 @@ const Home = ()=>{
 
 
     return(
-        <div>
+        <div className="homepage">
             <p>home page</p>
-            <button><Link to='/main_app'>Start</Link></button>
+            <Link to='/main_app'><button>Start</button></Link>
             <PopUp trigger={timedPopup} setTrigger={setTimedPopup}>
                 <h3>Pop-up</h3>
             </PopUp>
